d75695a5619c7c36c69c58bdabb0479960539bc7
Cloud User <centos@ip-10-10-44-68.eu-west-1.compute.internal>
2016-11-08 13:49:24 +0000

    color

--- first-doc/docs/conf.py
+++ first-doc/docs/conf.py
@@ -108,7 +108,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-html_theme = 'classic'
+html_theme = 'sphinxdoc'
 #on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 #if not on_rtd:  # only import and set the theme if we're building docs locally
 #    import sphinx_rtd_theme
